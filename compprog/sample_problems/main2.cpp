1d8ca91f72552a520e49c85bb7a7fb9bbf8ded61
iftekharx <iftekharx86@gmail.com>
2023-05-02 18:06:58 +0600

    question 2 solved

---
+++ compprog/sample_problems/main2.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// PROBLEM NAME: Jiyas Sequence
+
+
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        long long product = 1;
+        for(int i = 0; i < n; ++i)
+        {
+            int x;
+            cin >> x;
+            product = product * x;
+        }
+
+        int lsd = product % 10;
+        if(lsd == 2 || lsd == 3 || lsd == 5)
+        {
+            cout << "YES" << endl;
+        }else
+        {
+            cout << "NO" << endl;
+        }
+    }
+
+
+}
