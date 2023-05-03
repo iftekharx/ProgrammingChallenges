5013959871594b161f1d4fc18784f92825a39c28
iftekharx <iftekharx86@gmail.com>
2023-05-03 09:36:16 +0600

    problem 4 solved

---
+++ compprog/sample_problems/main4.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// PROBLEM: CITY TOUR
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while(t--){
+        long long n;
+        cin >> n;
+        cout << 4 * n << endl;
+    }
+
+    
+
+}
